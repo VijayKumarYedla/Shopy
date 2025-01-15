296a1b755e4e0a9d3c5a127e8d0478e36293f86d
VijayKumarYedla <vijaykumar95534@gmail.com>
2025-01-15 20:28:00 +0530

    changes in .env file

--- Shopy/settings.py
+++ Shopy/settings.py
@@ -11,8 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+DEBUG=False
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 ALLOWED_HOSTS = ['shopy-4m0v.onrender.com', 'localhost', '127.0.0.1']  # Add your Render host here
 
