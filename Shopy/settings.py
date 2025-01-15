5df437fe52e1cf9030e55f03e8d9f6e0d6d8444d
VijayKumarYedla <vijaykumar95534@gmail.com>
2025-01-15 13:26:13 +0530

    Updated ALLOWED_HOSTS to include live domain

--- Shopy/settings.py
+++ Shopy/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = ['shopy-4m0v.onrender.com']
+
 
 # Application definition
 INSTALLED_APPS = [
